cf55994a65751e6c0f493e64d6a6590edfe165a6
Manoj Kumar Ramamurthy <rmk7382@gmail.com>
2025-08-25 21:28:31 +0530

    added view counter

--- script.js
+++ script.js
@@ -27,3 +27,11 @@ function createParticles() {
   }
 }
 createParticles();
+
+// Javascript code
+const counter = document.querySelector(".counter-number");
+async function updateCounter(){
+    let response = await fetch("https://o25hc5nrjoxfc7anqbnfq3gyui0jptxw.lambda-url.ap-south-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
