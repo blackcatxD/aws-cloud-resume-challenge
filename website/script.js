76543896c3926c2b5e0233786cca89924893e99f
Manoj Kumar Ramamurthy <rmk7382@gmail.com>
2025-08-26 12:53:53 +0530

    init

--- website/script.js
+++ website/script.js
@@ -33,7 +33,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter(){
     let response = await fetch("https://o25hc5nrjoxfc7anqbnfq3gyui0jptxw.lambda-url.ap-south-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ` Views: ${data}`;
+    counter.innerHTML = `Total views to the webpage: ${data}`;
 }
 
 updateCounter();
