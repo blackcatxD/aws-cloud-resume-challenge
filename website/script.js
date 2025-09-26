a53feabf2d32b0f80b9a3296d21faf5bc3058cdc
Manoj Kumar Ramamurthy <rmk7382@gmail.com>
2025-09-26 14:02:37 +0530

    optimization

--- website/script.js
+++ website/script.js
@@ -165,3 +165,13 @@ document.querySelector('.back-to-top').addEventListener('click', () => {
 window.addEventListener('load', () => {
   document.querySelector('.preloader').classList.add('hidden');
 });
+
+// New: Smooth scroll for anchors
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
